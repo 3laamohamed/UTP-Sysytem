4f3edc673e795d69179b868c54344cd84d4fd884
Al7awy <ibrahimfares511@gmail.com>
2022-10-04 22:14:08 +0200

    Faq page

--- resources/views/admin/faq.blade.php
+++ resources/views/admin/faq.blade.php
@@ -15,7 +15,7 @@
             <textarea class="form-control mt-3" placeholder="Write Your Answer" id="answer" style="height: 200px"></textarea>
           </div>
           <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-success clicked" id="save_copy_right" type="button">Save</button>
+            <button class="btn btn-success clicked" id="save_faq" type="button">Save</button>
           </div>
         </div>
         <table class="table mt-4 text-center shadow-lg">
@@ -47,7 +47,123 @@
         </table>
       </div>
 </div>
-  <script>
+<script>
 
-  </script>
+let _token = $('input[name="_token"]').val(),
+  action = "",
+  mood = "create";
+$("body").on("click", "#save_faq", function () {
+  let question = $("#question").val();
+  let answer = $("#answer").val();
+  action = "save";
+  let b = $("tbody").html();
+  $.ajax({
+    url: "{{route('admin.save_faq')}}",
+    method: "post",
+    enctype: "multipart/form-data",
+    data: { _token,question,answer,action },
+    success: function (c) {
+      if (c.status == "true") {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Saved Question",
+          showConfirmButton: !1,
+          timer: 1500,
+        }),
+        (b += `<tr id="${c.msg}">
+          <td>${c.msg}</td>
+          <td>${question}</td>
+          <td>${answer}</td>
+          <td>
+            <button class="table-buttons" onclick="getRow(${c.msg})"><ion-icon class="text-primary" name="create-outline"></ion-icon></button>
+            <button class="table-buttons" id='delete_group'><ion-icon class="text-danger" name="trash-outline"></ion-icon></button>
+          </td>
+        </tr>`),
+        $("tbody").html(b),
+        $("#question").val("")
+        $("#answer").val("")
+      }
+    },
+  });
+}),
+$("body").on("click", "#update_faq", function () {
+  let question = $("#question");
+  let answer = $("#answer").val();
+  action = "update"
+  $.ajax({
+    url: "{{route('admin.save_faq')}}",
+    method: "post",
+    enctype: "multipart/form-data",
+    data: { _token, questionId: question.attr("row"), question: question.val(),answer, action },
+    success: function (b) {
+      if(b.status == "true") {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: b.msg,
+          showConfirmButton: !1,
+          timer: 1500,
+        }),
+        $(`tr#${question.attr("row")}`)
+          .find("td:nth-child(2)")
+          .text(question.val()),
+          .find("td:nth-child(3)")
+          .text(answer),
+        mood = "create",
+        $("#update_faq")
+          .html("create" === mood ? "Save" : "Update")
+          .removeClass("btn-primary")
+          .addClass("btn-success")
+          .attr("id", "save_faq"),
+          question.val("");
+          $("#answer").val("");
+      }
+    },
+  });
+}),
+$("body").on("click", "#delete_faq", function () {
+  action = "del";
+  let a = $(this).parents("tr").attr("id");
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't delete this Question",
+    icon: "warning",
+    showCancelButton: !0,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "e#d33",
+    confirmButtonText: "Yes, dlete it!",
+  }).then((b) => {
+    b.isConfirmed &&
+      $.ajax({
+        url: "{{route('admin.delete_faq')}}",
+        method: "post",
+        enctype: "multipart/form-data",
+        data: { _token, questionId: a, action },
+        success: function (b) {
+          $(`tr#${a}`).remove(),
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: b.msg,
+              showConfirmButton: !1,
+              timer: 1500,
+            });
+        },
+      });
+  });
+});
+function getRow(a) {
+  let question = $(`tr#${a}`).find("td:nth-child(2)").text();
+  let answer = $(`tr#${a}`).find("td:nth-child(3)").text();
+  $("#question").val(question).focus().attr("row", a);
+  $("#answer").val(answer).focus();
+  mood = "update"
+  $("#save_faq")
+    .html("update" === mood ? "Update" : "Save")
+    .addClass("btn-primary")
+    .removeClass("btn-success")
+    .attr("id", "update_faq");
+}
+</script>
 @stop
